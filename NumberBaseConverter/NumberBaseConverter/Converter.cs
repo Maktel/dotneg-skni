f73fb0dc2a8af3d237460e25f54e4510833eefc2
marek.luszczki <marek.luszczki@skni.umcs.pl>
2018-04-05 11:44:12 +0200

    added NumberBaseConverter solution

---
+++ NumberBaseConverter/NumberBaseConverter/Converter.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace NumberBaseConverter
+{
+    internal class Converter
+    {
+        private string Digits { get; set; }
+
+        public Converter(string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        {
+            Digits = digits;
+        }
+
+        public string ConvertToTextRepresentationInAnyBase(int number, int @base)
+        {
+            if (@base > Digits.Length)
+            {
+                throw new Exception(
+                    $"The given base: {@base} is too large to be represented with digits + letters of the English alphabet.");
+            }
+
+            if (@base < 2)
+            {
+                throw new Exception($"The given base: {@base} must be equal or greater than 2.");
+            }
+
+            if (number < 0)
+            {
+                throw new Exception("Converting negative numbers is not supported for the time being.");
+            }
+
+            if (number == 0)
+            {
+                return "0";
+            }
+
+            string representation = "";
+
+
+            while (number > 0)
+            {
+                representation += Digits[number % @base];
+                number /= @base;
+            }
+
+            // the most upvoted answer from SO
+            //            char[] reverse = representation.ToCharArray();
+            //            Array.Reverse(reverse);
+            //            return new String(reverse);
+
+            // one-liner
+            return new string(representation.Reverse().ToArray());
+        }
+
+        public string ConvertToTextRepresentationInBase16(int number)
+        {
+            return ConvertToTextRepresentationInAnyBase(number, 16);
+        }
+
+        public string ConvertToTextRepresentationInBase11(int number)
+        {
+            return ConvertToTextRepresentationInAnyBase(number, 11);
+        }
+
+
+        /* Unit tests */
+        public void TestCorrectRepresentationInBase11()
+        {
+            const int number = 1263;
+
+            string representation = ConvertToTextRepresentationInBase11(number);
+
+            Debug.Assert(representation.Equals("A49"));
+        }
+
+        public void TestCorrectRepresentationInBase16()
+        {
+            const int number = 1263;
+
+            string representation = ConvertToTextRepresentationInBase16(number);
+
+            Debug.Assert(representation.Equals("4EF"));
+        }
+
+        public void TestCorrectRepresentationInBase2()
+        {
+            const int number = 1263;
+
+            string representation = ConvertToTextRepresentationInAnyBase(number, 2);
+
+            Debug.Assert(representation.Equals("10011101111"));
+        }
+
+        public void TestTooLargeBase()
+        {
+            const int number = 0;
+            const int @base = 56;
+
+            try
+            {
+                string representation = ConvertToTextRepresentationInAnyBase(number, @base);
+                Debug.Assert(false);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+    }
+}
---
+++ NumberBaseConverter/NumberBaseConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumberBaseConverter
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            //            converter.TestCorrectRepresentationInBase11();
+            //            converter.TestCorrectRepresentationInBase16();
+            //            converter.TestCorrectRepresentationInBase2();
+            //            converter.TestTooLargeBase();
+
+            ConvertFromUserInput();
+        }
+
+        private static void ConvertFromUserInput()
+        {
+            Converter converter = new Converter();
+
+            int linesCount = Convert.ToInt32(Console.ReadLine());
+            while (linesCount-- > 0)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("base 10: {0}", number);
+                Console.WriteLine("base  2: {0}", converter.ConvertToTextRepresentationInAnyBase(number, 2));
+                Console.WriteLine("base 11: {0}", converter.ConvertToTextRepresentationInBase11(number));
+                Console.WriteLine("base 16: {0}", converter.ConvertToTextRepresentationInBase16(number));
+            }
+        }
+    }
+}
